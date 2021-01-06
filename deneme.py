e7079bf38084f3b2c15414b7407633eb50b5e892
kadirkayhann <77049603+kadirkayhann@users.noreply.github.com>
2021-01-06 17:47:55 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,40 +1,62 @@
-import random
+class insan():
+    def __init__(self, boy, saç, saç_rengi):
+        self.boy = boy
+        self.saç = saç
+        self.saç_rengi = saç_rengi
 
-class Asker:
+    def giris(self):
+        metin="""lütfen islem yapmak için sayi girininiz
+        1-boy
+        2-saç 
+        3- saç rengi
+        sayi:"""
+        veri=int(input(metin))
+        if veri == 1 :
+           self.boyu_yazdir()
+        if veri == 2 :
+            self.saç_yazdir()
+        if veri == 3 :
+            self.saç_rengi_yazdir()
+    def boy_sorma (self):
+        boy_sor=int(input("lütfen kullanicinin adını giriniz"))
+        if boy_sor == "yunus" :
+            yunus.boyu_yazdir()
+        if boy_sor == "kadir" :
+            kadir.boyu_yazdir()
 
-    def __init__(self, isim, can):
-        self.isim = isim
-        self.can = can
-        self.sarjor = 30
+    def saç_sorma(self):
+        saç_sor=int(input("lütfen kullanicinin adını giriniz"))
+        if saç_sor == "yunus" :
+            yunus.saç_yazdir()
+        if saç_sor == "kadir" :
+            kadir.saç_yazdir
+    def saç_rengi_sorma(self):
+        saç_rengi_sor=int(input("lütfen kullanicinin adını giriniz"))
+        if saç_rengi_sor == "yunus":
+            yunus.saç_rengi_yazdir()
+        if saç_rengi_sor == "kadir" :
+            kadir.saç_rengi_yazdir()
 
-    def saldir(self):
-        self.mermi_gucu = random.randint(5, 10)
-        harcanan_mermi = random.randint(1, 6)
-        self.sarjor = self.sarjor - harcanan_mermi
-        return harcanan_mermi * self.mermi_gucu
+    def boyu_yazdir (self):
+        print(self.boy)
+    def saç_yazdir (self):
+        print(self.saç)
+    def saç_rengi_yazdir(self):
+        print(self.saç_rengi)
+def giriss():
+        naber="""lütfen islem yapmak için sayi giriniz
+        1-yunus 
+        2-kadir
+        sayi:"""
+        iyi=int(input(naber))
+        if iyi == 1 :
+            yunus.giris()
+        if iyi == 2 :
+            kadir.giris()
+
+yunus = insan("220","uzun","mavi")
+kadir= insan ("230","kısa","beyaz")
 
 
-class Dusman:
 
-    def __init__(self, isim, can):
-        self.isim = isim
-        self.can = can
-        self.sarjor = 30
-
-    def saldir(self):
-        self.mermi_gucu = random.randint(5, 10)
-        harcanan_mermi = random.randint(1, 6)
-        self.sarjor = self.sarjor - harcanan_mermi
-        return harcanan_mermi * self.mermi_gucu
-
-    def savun(self, gelen_hasar):
-        savunulan_hasar = random.randint(0, gelen_hasar)        
-        self.can = self.can - (gelen_hasar - savunulan_hasar)
-        return self.can
-
-asker = Asker('isim', 100)
-dusman = Dusman('isim', 100)
-
-a = asker.saldir()
-print(a)
-print(dusman.savun(a))
+giriss()
